deee95b213f14bfc42760c674dc6b11abd0dd56d
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-02-08 12:02:33 +0100

    sending json data first endpoint get

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const fortunes = require('./data/fortunes.json')
 const port = 3000;
 const app = express();
 
 
 //ENDPOINTS
 app.get('/fortunes', (req, res) => {
-    console.log("requesting fortunes")
+    res.json(fortunes)
+    //Showing the json data from fortunes with res.json(MyVariableThatHasJsonData)
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
