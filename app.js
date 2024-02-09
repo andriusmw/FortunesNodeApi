9b3534641426fd923442ba25c23819ad501a90ff
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-02-09 08:43:57 +0100

    comments

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const fortunes = require('./data/fortunes.json')
 const app = express();
 
 
-//ENDPOINTS
+//---------------------------------ENDPOINTS--------------------------------------------
+//--------------------------------------------------------------------------------------
+//*Note: instead of declaring the function here we should declarate functions on other file,
+//import that file and calling the functions here after the endpoint so we have the code clean.
+
+//get all fortunes
 app.get('/fortunes', (req, res) => {
     res.json(fortunes)
     //Showing the json data from fortunes with res.json(MyVariableThatHasJsonData)
 });
 
+//get a random fortune cookie
 app.get('/fortunes/random', (req,res) => {
     console.log("requesting random fortune");
 
